75c98f16ce9828bf70d1d6a69eeec94fb40128ae
Oscar Morlet <oscarmorletdev@gmail.com>
2024-11-25 01:46:08 -0600

    feat: add clear list button

--- src/components/ColorList.jsx
+++ src/components/ColorList.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Swal from "sweetalert2";
 
-const ColorList = ({ colorList }) => {
+const ColorList = ({ colorList = [], handleClickClearColors }) => {
   const handleCopyColor = (color) => {
     navigator.clipboard.writeText(color);
     Swal.fire({
@@ -12,32 +12,43 @@ const ColorList = ({ colorList }) => {
       timer: 1300,
       timerProgressBar: true,
     });
-  }
+  };
   return (
-    <div className="list-group text-center">
-      {colorList?.length > 0 ? (
-        colorList.map((color, index) => (
-          <button
-            key={index}
-            type="button"
-            className="list-group-item list-group-item-action text-white"
-            aria-current="true"
-            title="Copiar..."
-            style={{
-              background: color,
-              fontWeight: "bolder",
-            }}
-            onClick={() => handleCopyColor(color)}
-          >
-            {color}
-          </button>
-        ))
-      ) : (
-        <div className="alert alert-danger" role="alert">
-          <b>Agrega un color...</b>
-        </div>
+    <>
+      {colorList.length > 0 && (
+        <button
+          className="btn btn-danger my-4 w-100"
+          onClick={handleClickClearColors}
+        >
+          Clear List
+        </button>
       )}
-    </div>
+
+      <div className="list-group text-center">
+        {colorList?.length > 0 ? (
+          colorList.map((color, index) => (
+            <button
+              key={index}
+              type="button"
+              className="list-group-item list-group-item-action text-white"
+              aria-current="true"
+              title="Copiar..."
+              style={{
+                background: color,
+                fontWeight: "bolder",
+              }}
+              onClick={() => handleCopyColor(color)}
+            >
+              {color}
+            </button>
+          ))
+        ) : (
+          <div className="alert alert-danger" role="alert">
+            <b>Agrega un color...</b>
+          </div>
+        )}
+      </div>
+    </>
   );
 };
 
